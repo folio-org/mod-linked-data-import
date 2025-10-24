6ab7898d22df67f989dd80fb43484006c7dbac4c
PBobylev <pavel_bobylev@epam.com>
2025-10-24 15:37:00 +0500

    MODLDI-8: migrate Spring Batch init script to Liquibase

--- src/main/java/org/folio/linked/data/imprt/service/tenant/LinkedDataImportTenantService.java
+++ src/main/java/org/folio/linked/data/imprt/service/tenant/LinkedDataImportTenantService.java
@@ -1,17 +1,12 @@
 package org.folio.linked.data.imprt.service.tenant;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.stream.Collectors;
 import lombok.extern.log4j.Log4j2;
 import org.folio.spring.FolioExecutionContext;
-import org.folio.spring.exception.FolioContextExecutionException;
 import org.folio.spring.liquibase.FolioSpringLiquibase;
 import org.folio.spring.service.TenantService;
 import org.folio.spring.tools.kafka.KafkaAdminService;
 import org.folio.tenant.domain.dto.TenantAttributes;
 import org.springframework.context.annotation.Primary;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
@@ -38,22 +33,8 @@ public class LinkedDataImportTenantService extends TenantService {
   }
 
   @Override
-  protected void afterLiquibaseUpdate(TenantAttributes tenantAttributes) {
-    log.info("Creating Spring Batch DB tables for tenant {}", context.getTenantId());
-    jdbcTemplate.execute(readSpringBatchSchemaSql());
-  }
-
-  @Override
   protected void afterTenantDeletion(TenantAttributes tenantAttributes) {
     kafkaAdminService.deleteTopics(context.getTenantId());
   }
 
-  private String readSpringBatchSchemaSql() {
-    var resource = new ClassPathResource("org/springframework/batch/core/schema-postgresql.sql");
-    try (var reader = new BufferedReader(new InputStreamReader(resource.getInputStream()))) {
-      return reader.lines().collect(Collectors.joining("\n"));
-    } catch (Exception e) {
-      throw new FolioContextExecutionException("Error reading Spring Batch SQL file", e);
-    }
-  }
 }
