622b1f744d82aaf89b7b35e8b20cab3672fa277a
PBobylev <pavel_bobylev@epam.com>
2025-11-06 18:05:54 +0500

    MODLDI-7: LinkedDataServiceImpl fix

--- src/main/java/org/folio/linked/data/imprt/integration/client/LinkedDataClient.java
+++ src/main/java/org/folio/linked/data/imprt/integration/client/LinkedDataClient.java
@@ -1,5 +1,7 @@
 package org.folio.linked.data.imprt.integration.client;
 
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+
 import java.util.Set;
 import org.folio.ld.dictionary.model.Resource;
 import org.folio.linked.data.imprt.domain.dto.SearchResourcesRequestDto;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 @FeignClient(name = "linked-data")
 public interface LinkedDataClient {
 
-  @PostMapping(value = "/resources/search")
+  @PostMapping(value = "/resources/search", produces = APPLICATION_JSON_VALUE)
   ResponseEntity<Set<Resource>> searchResources(SearchResourcesRequestDto searchRequest);
 
 }
--- src/main/java/org/folio/linked/data/imprt/integration/ld/LinkedDataServiceImpl.java
+++ src/main/java/org/folio/linked/data/imprt/integration/ld/LinkedDataServiceImpl.java
@@ -2,15 +2,13 @@ package org.folio.linked.data.imprt.integration.ld;
 
 import static java.util.Optional.ofNullable;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.Collection;
 import java.util.Set;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.folio.ld.dictionary.model.Resource;
 import org.folio.linked.data.imprt.domain.dto.SearchResourcesRequestDto;
 import org.folio.linked.data.imprt.integration.client.LinkedDataClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Log4j2
@@ -18,16 +16,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class LinkedDataServiceImpl implements LinkedDataService {
 
-  private final ObjectMapper objectMapper;
   private final LinkedDataClient linkedDataClient;
 
   @Override
   public Set<Resource> searchResources(Set<String> inventoryIds) {
-    return ofNullable(linkedDataClient.searchResources(new SearchResourcesRequestDto(inventoryIds))
-      .getBody())
-      .stream()
-      .flatMap(Collection::stream)
-      .collect(Collectors.toSet());
+    return ofNullable(linkedDataClient.searchResources(new SearchResourcesRequestDto(inventoryIds)))
+      .map(ResponseEntity::getBody)
+      .orElse(Set.of());
   }
 
 }
