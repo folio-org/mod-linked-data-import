1e4ab52a8d55e1b8a847d28dc0505b4721ffe9dd
PBobylev <pavel_bobylev@epam.com>
2025-11-10 16:37:32 +0500

    MODLDI-7: jobInstanceId added to ImportOutput DTO

--- src/main/java/org/folio/linked/data/imprt/batch/job/writer/LdKafkaSender.java
+++ src/main/java/org/folio/linked/data/imprt/batch/job/writer/LdKafkaSender.java
@@ -3,7 +3,6 @@ package org.folio.linked.data.imprt.batch.job.writer;
 import static org.folio.linked.data.imprt.util.CollectionUtil.chunked;
 
 import java.util.Set;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.folio.ld.dictionary.model.Resource;
 import org.folio.linked.data.imprt.domain.dto.ImportOutput;
@@ -18,18 +17,25 @@ import org.springframework.stereotype.Component;
 @Log4j2
 @Component
 @StepScope
-@RequiredArgsConstructor
 public class LdKafkaSender implements ItemWriter<Set<Resource>> {
 
-  @Qualifier("importOutputMessageProducer")
+  private final Long jobInstanceId;
   private final FolioMessageProducer<ImportOutput> importOutputFolioMessageProducer;
-  @Value("${mod-linked-data-import.output-chunk-size}")
-  private Integer chunkSize;
+  private final Integer chunkSize;
+
+  public LdKafkaSender(@Value("#{jobInstanceId}") Long jobInstanceId,
+                       @Qualifier("importOutputMessageProducer") FolioMessageProducer<ImportOutput> producer,
+                       @Value("${mod-linked-data-import.output-chunk-size}") Integer chunkSize) {
+    this.jobInstanceId = jobInstanceId;
+    this.importOutputFolioMessageProducer = producer;
+    this.chunkSize = chunkSize;
+  }
+
 
   @Override
   public void write(Chunk<? extends Set<Resource>> chunk) {
     var messages = chunked(chunk.getItems().stream().flatMap(Set::stream), chunkSize)
-      .map(ImportOutput::new)
+      .map(resources -> new ImportOutput(resources).jobInstanceId(jobInstanceId))
       .toList();
     importOutputFolioMessageProducer.sendMessages(messages);
   }
